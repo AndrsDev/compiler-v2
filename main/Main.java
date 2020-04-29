1e18b99695f7c210d430341832e4d956afcb3079
Andres Sanabria <andrs.dev@gmail.com>
2020-04-29 10:03:36 -0600

    lexical and sintax analyzer finished

--- main/Main.java
+++ main/Main.java
@@ -9,9 +9,9 @@ import java.io .* ;
 public class Main {
     public static void main(String[] args) throws IOException { 
         // AnalizadorLexico lex = new AnalizadorLexico();
-        // Analizador analizar = new A nalizador(lex);
-        // analizar.programaO; 
-        // System.out.writeC’Xn’);
+        // Analizador analizar = new Analizador(lex);
+        // analizar.programa(); 
+        // System.out.write(’\n’);
         System.out.println("testing my compiler");
     }
 }
