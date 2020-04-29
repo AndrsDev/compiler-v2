31833c52f87024c31f19c1a64e813c108a72beca
Andres Sanabria <andrs.dev@gmail.com>
2020-04-29 08:21:09 -0600

    initial commit

---
+++ main/Main.java
@@ -0,0 +1,17 @@
+package main;
+import java.io .* ; 
+
+// import analizadorLexico.* ; 
+// import analizador.* ;
+// import java.io.*;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException { 
+        // AnalizadorLexico lex = new AnalizadorLexico();
+        // Analizador analizar = new A nalizador(lex);
+        // analizar.programaO; 
+        // System.out.writeC’Xn’);
+        System.out.println("testing my compiler");
+    }
+}
