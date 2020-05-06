a1f113681f162fc6d5d3b2c0e1ae507881606e9b
Andres Sanabria <andrs.dev@gmail.com>
2020-05-06 13:21:55 -0600

    code fixes

--- main/Main.java
+++ main/Main.java
@@ -11,7 +11,6 @@ import analizador.* ;
 
 public class Main {
     public static void main(String[] args) throws IOException { 
-
         System.out.println();
         System.out.println("Ingresa el codigo y al terminar ingresa DONE: ");
         AnalizadorLexico lex = new AnalizadorLexico();
