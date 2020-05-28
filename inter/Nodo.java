36280a8a88309206509c58be413a014c7a2c821f
Andres Sanabria <andrs.dev@gmail.com>
2020-05-28 09:21:40 -0600

    finished destination code

--- inter/Nodo.java
+++ inter/Nodo.java
@@ -1,27 +1,72 @@
 package inter;
+
 import analizadorLexico.*;
+import java.io.*;
 
 public class Nodo {
     int linealex = 0;
     static int etiquetas = 0;
-    
+
     Nodo() {
         linealex = AnalizadorLexico.linea;
     }
 
     void error(String s) {
-        throw new Error("cerca de la linea "+linealex+ ": "+ s);
+        throw new Error("cerca de la linea " + linealex + ": " + s);
     }
 
-    public int nuevaEtiqueta(){
+    String operandos(String s){
+        String result = "";
+        String[] parts = s.split("=");
+        String operation = parts[1];
+        String[] items = operation.split(" ");
+
+        for (int i = 0; i < items.length; i++) {
+            if(items[i].matches("^[_a-z]\\w*$")){
+                result+= items[i] + ", ";
+            }
+        }
+        
+        return result;
+    }
+
+    void obtenReg(String s) {
+        try {
+            String line = "";
+            if(s.contains("=")){
+
+                if(s.contains("+")){
+                    line+= "ADD " + operandos(s);
+                } else if(s.contains("-")){
+                    line+= "SUB " + operandos(s);
+                } else if(s.contains("*")){
+                    line+= "MUL " + operandos(s);
+                } else if(s.contains("/")){
+                    line+= "DIV " + operandos(s);
+                }
+
+            } else {
+                line+= s;
+            }
+
+            FileWriter fw = new FileWriter("./main/output.txt", true);
+            fw.write(line + '\n');
+            fw.close();
+        } catch (Exception e) {
+            //Didn't write to file
+        }
+    }
+
+    public int nuevaEtiqueta() {
         return ++etiquetas;
     }
 
-    public void emitirEtiqueta(int i){
+    public void emitirEtiqueta(int i) {
         System.out.print("L" + i + ":");
     }
 
-    public void emitir(String s){
+    public void emitir(String s) {
+        obtenReg(s);
         System.out.println("\t" + s);
     }
 }
--- main/Main.java
+++ main/Main.java
@@ -11,11 +11,22 @@ import analizador.* ;
 
 public class Main {
     public static void main(String[] args) throws IOException { 
+        cleanOutput();
         System.out.println();
         System.out.println("Ingresa el codigo y al terminar ingresa DONE: ");
         AnalizadorLexico lex = new AnalizadorLexico();
         Analizador analizar = new Analizador(lex);
         analizar.programa(); 
         System.out.write('\n');
+    }
+
+    static void cleanOutput(){
+        try {
+            FileWriter fw = new FileWriter("./main/output.txt");
+            fw.write("");
+            fw.close();
+        } catch (Exception e) {
+            //File not found.
+        }
     }
 }
